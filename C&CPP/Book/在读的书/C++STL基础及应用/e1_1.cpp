a4d1e469d61467ad4951dbbbec697c4a81ed2f31
wolflion <successwolf@163.com>
2016-04-25 07:28:56 +0800

    namespace
    namespace的例子

---
+++ C&CPP/Book/在读的书/C++STL基础及应用/e1_1.cpp
@@ -0,0 +1,36 @@
+/*
+ * 20160424
+ * Àı1.1 namespaceÃüÃû¿Õ¼äÊ¾Àı
+*/
+
+#include <iostream>
+namespace mycompany
+{
+	void func()
+	{
+		std::cout << "Hello, this is my company" << std::endl;
+	}
+}
+
+namespace yourcompany
+{
+	void func()
+	{
+		std::cout << "Hello, this is your company" << std::endl;
+	}
+}
+
+using namespace mycompany;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	func();
+	std::cout << std::endl;
+
+	//yourcompany::func();
+	//cout << endl ;
+
+	system("pause");
+	return 0;
+}
